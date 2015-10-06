7d5bd2cb06a4076d52e271cf3523a43838477032
Maksadbek <a.maksadbek@gmail.com>
2015-10-06 17:05:52 +0500

    mongodb connection package

--- mongostore/mongostore.go
+++ mongostore/mongostore.go
@@ -1,23 +1,38 @@
 package mongostore
 
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	MONGODSN       string = "localhost"
+	DBNAME         string = "urlshortener"
+	COLLECTIONNAME string = "urlcollection"
+)
+
 type MongoConn struct {
 	session *mgo.Session
 }
 
 type MongoDoc struct {
-	ID       bson.ObjectID `bson:"_id"`
+	ID       bson.ObjectId `bson:"_id"`
 	ShortURL string        `bson:"shortURL"`
 	LongURL  string        `bson:"longURL"`
 }
 
-func (mc *MongoConnection) Connect() error {
+func (mc *MongoConn) Connect() (err error) {
 	log.Println("connecting to MongoDB...")
 	mc.session, err = mgo.Dial(MONGODSN)
 	if err != nil {
 		return fmt.Errorf("could not connect to MongoDB server")
 	}
 	log.Println("successfully connected to MongoDB server")
-	URLCollection := c.session.DB(DBNAME).C(COLLECTIONNAME)
+	URLCollection := mc.session.DB(DBNAME).C(COLLECTIONNAME)
 	if URLCollection == nil {
 		err = errors.New("could not create collection")
 	}
@@ -30,17 +45,17 @@ func (mc *MongoConnection) Connect() error {
 	return nil
 }
 
-func (mc *MongoConnection) GetSessionAndCollection() (session *mgo.Session, collection *mgo.Collection, error err) {
+func (mc *MongoConn) GetSessionAndCollection() (session *mgo.Session, collection *mgo.Collection, err error) {
 	if mc.session != nil {
-		session := mc.session.Copy()
-		URLCollection = session.DB(DBNAME).C(COLLECTIONNAME)
+		session = mc.session.Copy()
+		collection = session.DB(DBNAME).C(COLLECTIONNAME)
 	} else {
 		err = errors.New("session is nil")
 	}
-	return
+	return session, collection, err
 }
 
-func (mc *MongoConnection) AddURLs(longURL string, shortURL string) error {
+func (mc *MongoConn) AddURLs(longURL string, shortURL string) error {
 	session, URLCollection, err := mc.GetSessionAndCollection()
 	if err != nil {
 		log.Println("could not get sessino and collection")
@@ -48,8 +63,8 @@ func (mc *MongoConnection) AddURLs(longURL string, shortURL string) error {
 	}
 	defer session.Close()
 	err = URLCollection.Insert(
-		&MongoDOC{
-			ID:       bson.NewObjectID(),
+		&MongoDoc{
+			ID:       bson.NewObjectId(),
 			ShortURL: shortURL,
 			LongURL:  longURL,
 		},
@@ -57,8 +72,24 @@ func (mc *MongoConnection) AddURLs(longURL string, shortURL string) error {
 	if err != nil {
 		// check for duplicate
 		if mgo.IsDup(err) {
-			return errors.New("duplicate entry for this short url %s", shortURL)
+			return fmt.Errorf("duplicate entry for this short url %s", shortURL)
 		}
 	}
 	return nil
+}
+
+func (mc *MongoConn) FindLongURL(shortURL string) (string, error) {
+	result := MongoDoc{}
+	session, collection, err := mc.GetSessionAndCollection()
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+
+	err = collection.Find(bson.M{"shortURL": shortURL}).One(&result)
+	if err != nil {
+		return "", err
+	}
+
+	return result.LongURL, nil
 }
